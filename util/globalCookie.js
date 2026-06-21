152ad447a6aba0d5e85e21382abc75feecf55c29
xihuanyu09 <xhy15230195597@gmail.com>
2026-06-21 14:16:23 +0800

    Update globalCookie.js

--- util/globalCookie.js
+++ util/globalCookie.js
@@ -1,23 +1,42 @@
+const jsonFile = require('jsonfile');
+
+function parseCookieStr(str) {
+  const userCookie = {};
+  str.split('; ').filter(Boolean).forEach((c) => {
+    const i = c.indexOf('=');
+    if (i > 0) userCookie[c.slice(0, i)] = c.slice(i + 1);
+  });
+  if (Number(userCookie.login_type) === 2) {
+    userCookie.uin = userCookie.wxuin;
+  }
+  userCookie.uin = (userCookie.uin || '').replace(/\D/g, '');
+  return userCookie;
+}
+
 module.exports = () => {
   let allCookies = {};
   let userCookie = {};
 
-  try {
-    allCookies = jsonFile.readFileSync('data/allCookies.json')
-  } catch (err) {
-    // get allCookies failed
+  if (process.env.QQ_COOKIE?.trim()) {
+    userCookie = parseCookieStr(process.env.QQ_COOKIE.trim());
+  } else {
+    try {
+      userCookie = jsonFile.readFileSync('data/cookie.json');
+    } catch (err) {
+      // no cookie file
+    }
   }
 
   try {
-    userCookie = jsonFile.readFileSync('data/cookie.json')
+    allCookies = jsonFile.readFileSync('data/allCookies.json');
   } catch (err) {
-    // get cookie failed
+    // no allCookies file
   }
 
   return {
     allCookies: () => allCookies,
     userCookie: () => userCookie,
-    updateAllCookies: (v) => allCookies = v,
-    updateUserCookie: (v) => userCookie = v,
-  }
-}
+    updateAllCookies: (v) => { allCookies = v; },
+    updateUserCookie: (v) => { userCookie = v; },
+  };
+};
